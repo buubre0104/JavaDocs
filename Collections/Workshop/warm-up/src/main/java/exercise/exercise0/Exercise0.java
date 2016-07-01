96177e6c1338f6a4be56a75a0801d960a935df1e
buubre0104 <buu.bre0104@gmail.com>
2016-07-01 12:04:31 +0300

    day2 java

---
+++ Collections/Workshop/warm-up/src/main/java/exercise/exercise0/Exercise0.java
@@ -0,0 +1,46 @@
+package exercise.exercise0;
+import java.util.*;
+/**
+ * Created by Radu.Hoaghe on 4/20/2015.
+ *
+ * Exercise 0: Create a List (ArrayList or LinkedList), add elements to it and print all of them using ListIterator
+ *             for loop and foreach loop.
+ *
+ */
+public class Exercise0 {
+
+    public Exercise0(){
+
+    }
+
+    public void iterateThroughList(){
+
+        // TODO Exercise #0 a) Create a list (ArrayList or LinkedList) and add elements to it
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        // TODO Exercise #0 a) Don't forget to specify the type of the list (Integer, String etc.)
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        // TODO Exercise #0 b) Iterate through the list using ListIterator and print all its elements
+        ListIterator<Integer> it = array.listIterator();
+        while(it.hasNext()){
+                  System.out.println(it.next()+" ");
+        }
+        // TODO Exercise #0 c) Iterate through the list using classic for loop and print all its elements
+        for(int i=0; i< array.size(); i++){
+            System.out.println(array.get(i)+" ");
+        }
+
+        // TODO Exercise #0 d) Iterate through the list using foreach loop and print all its elements
+        for(Integer elem:array){
+            System.out.println(elem + " ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        // TODO Exercise #0 e) Create a new instance of Exercise0 class and call the iterateThroughList() method
+        Exercise0 instance = new Exercise0();
+        instance.iterateThroughList();
+    }
+}
---
+++ Collections/Workshop/warm-up/src/main/java/exercise/exercise3/Compara.java
@@ -0,0 +1,15 @@
+package exercise.exercise3;
+
+import java.util.Comparator;
+
+/**
+ * Created by user on 7/1/2016.
+ */
+public class Compara implements Comparator<String> {
+
+    public int compare(String o1, String o2) {
+        if(o1.length()>o2.length()) return 1;
+        if(o1.length()<o2.length()) return -1;
+        return 0;
+    }
+}
